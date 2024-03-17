49e3cb15bf0c01a9c59f1f8cb3fbaf0065b422b0
valentttino <vgboggio@gmail.com>
2024-03-16 22:40:01 -0300

    ex 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,10 +8,36 @@ const Btn = (props) =>{
   )
 }
 
-const Stcs = (props) => {
+const BtnStcs = (props) => {
   return(
     <p>
       {props.text}: {props.value}
+    </p>
+  )
+}
+
+const TotalComents = (props) =>{
+  return(
+    <p>
+      all: {props.ac}
+    </p>
+  )
+}
+
+const AvgComments = (props) =>{
+  const b = props.bad * (-1)
+  console.log('bad number * -1:', b)
+  return(
+    <p>
+      average: {(props.g + b)/props.a}
+    </p>
+  )
+}
+
+const PositiveComments = (props) =>{
+  return(
+    <p>
+      positive: {(props.g*100)/props.a}%
     </p>
   )
 }
@@ -20,25 +46,46 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allComents, setAll] = useState(0)
+
+  const goodClick = () =>{
+    setAll(allComents+1)
+    setGood(good+1)
+  }
+
+  const neutralClick = () =>{
+    setAll(allComents+1)
+    setNeutral(neutral+1)
+  }
+
+  const badClick = () =>{
+    setAll(allComents+1)
+    setBad(bad+1)
+  }
 
   return (
     <>
     <h1>
       Give feedback!
     </h1>
-    <Btn onClick={()=> setGood(good+1)} text={"good"} />
+    <Btn onClick={()=>goodClick()} text={"good"} />
 
-    <Btn onClick={()=> setNeutral(neutral+1)} text={"neutral"} />
+    <Btn onClick={()=>neutralClick()} text={"neutral"} />
 
-    <Btn onClick={()=> setBad(bad+1)} text={"bad"} />
+    <Btn onClick={()=>badClick()} text={"bad"} />
     
     <h2>
       Statistics
     </h2>
 
-    <Stcs text={"good"} value={good} />
-    <Stcs text={"neutral"} value={neutral} />
-    <Stcs text={"bad"} value={bad} />
+    <BtnStcs text={"good"} value={good} />
+    <BtnStcs text={"neutral"} value={neutral} />
+    <BtnStcs text={"bad"} value={bad} />
+    
+    <TotalComents ac={allComents} />
+    <AvgComments bad={bad} a={allComents} g={good} />
+    <PositiveComments g={good} a={allComents} />
+
     </>
   )
 }
