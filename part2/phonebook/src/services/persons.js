152f4f044e7e9ca6261ea424c06e91103aebd701
valentttino <vgboggio@gmail.com>
2024-05-03 00:23:10 -0300

    Ex 3.9 done. URLs modifieds for backend connection

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const pUrl = 'http://localhost:3001/persons'
+//const pUrl = 'http://localhost:3001/persons'
+const pUrl = 'http://localhost:3002/api/persons'
+
 
 const getAll = () => axios.get(pUrl)
 
@@ -8,7 +10,7 @@ const create = newObject => axios.post(pUrl, newObject)
 
 const update = (id, newObject) => axios.put(`${pUrl}/${id}`, newObject)
 
-const deletePerson = id => axios.delete(`http://localhost:3001/persons/${id}`)
+const deletePerson = id => axios.delete(`http://localhost:3002/persons/:${id}`)
 
 export default {getAll, create, update, deletePerson}
 
