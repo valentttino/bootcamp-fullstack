397fe175c0b7cb562561726b3bbf5acd2bb14c9c
valentttino <vgboggio@gmail.com>
2024-05-16 15:53:29 -0300

    updated users.js - users validation

--- part4/blogListApp/controllers/users.js
+++ part4/blogListApp/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const { error } = require('../utils/logger')
 
 usersRouter.get('/', async(req, res) =>{
     const users = await User.find({})
@@ -9,7 +10,14 @@ usersRouter.get('/', async(req, res) =>{
 
 usersRouter.post('/', async (req, res) => {
     const {username, name, password} = req.body
-
+    if(username === undefined && password === undefined){
+        return res.status(400).json({error: 'username and password missing'})
+    }else if (username === undefined){
+        return res.status(400).json({error: 'username missing'})
+    }else if (password === undefined){
+        return res.status(400).json({error: 'password missing'})
+    }
+    
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
