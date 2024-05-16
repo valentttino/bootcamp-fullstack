4ec57bd8e3d98dffec90bb9defb1617283d56fd1
valentttino <vgboggio@gmail.com>
2024-05-15 23:37:28 -0300

    added delete requests by id

--- part4/blogListApp/controllers/blogs.js
+++ part4/blogListApp/controllers/blogs.js
@@ -13,9 +13,13 @@ blogsRouter.get('/:id', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
 })
 
-  module.exports = blogsRouter
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+module.exports = blogsRouter
